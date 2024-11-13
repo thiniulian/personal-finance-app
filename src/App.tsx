bd45dd6c955c16ff9bbdc2e0d6c1a0982763376b
AnamariaGioada <anamaria.damaschin@gmail.com>
2024-11-13 18:14:39 +0200

    Ordered buttons

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,23 @@ import CaretRight from "./assets/images/icon-caret-right.svg?react";
 function App() {
   return (
     <>
-      <div>
-        <Button onClick={() => {}} disabled={false} className={"destroy"}>
-          <span>Placeholder</span>
-        </Button>
+      <Button onClick={() => {}} disabled={false} className={"primary"}>
+        <span>Placeholder</span>
+      </Button>
 
-        <Button onClick={() => {}} disabled={false} className={"primary"}>
-          <span>Placeholder</span>
-        </Button>
+      <Button onClick={() => {}} className={"secondary"}>
+        <span>Placeholder</span>
+      </Button>
+      <Button onClick={() => {}} className={"tertiary"}>
+        <span>
+          Placeholder
+          <CaretRight className="icon" />
+        </span>
+      </Button>
 
-        <Button onClick={() => {}} className={"secondary"}>
-          <span>Placeholder</span>
-        </Button>
-        <Button onClick={() => {}} className={"tertiary"}>
-          <span>Placeholder</span>
-          <span>
-            <CaretRight className="icon" />
-          </span>
-        </Button>
-      </div>
+      <Button onClick={() => {}} disabled={false} className={"destroy"}>
+        <span>Placeholder</span>
+      </Button>
     </>
   );
 }
