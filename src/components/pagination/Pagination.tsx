81bbd5cbf955188069ca06ae6c77bd9683d0a28f
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2024-11-15 12:55:05 +0200

    pagination styling done (no functionality yet)

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,29 +1,36 @@
+import { useState } from 'react';
 import './pagination.scss'; 
-// import Left from '../../assets/images/icon-caret-left.svg';
-import Right from '../../assets/images/icon-caret-right.svg';
+import Right from '../../assets/images/icon-caret-right.svg?react';
 import Left from '../../assets/images/icon-caret-left.svg?react';
-// import Logo from "./logo.svg?react"
 
+// TODO: functionalitate butoane
 export default function Pagination () {
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth); 
+    
+    window.addEventListener('resize', () => {
+        setViewportWidth(window.innerWidth);
+    })
+
     return(
         <div className='Pagination'>
             <button className='page-nav-btn'>
-                {/* <img src={Left} alt='Previous Button'/> */}
-                <Left/>
+                <Left role='img' aria-label='Previous'/>
+                { viewportWidth < 768 ? '' : 'Prev' }
+                {/* add text on prev and next buttons, when viewport width above tablet size */}
             </button>
 
             <div className='all-page-btns'>
                 <button className='page-btn'>1</button>
-                <button className='page-btn'>2</button>
+                <button className='page-btn active'>2</button>
                 <button className='page-btn'>3</button>
                 <button className='page-btn'>4</button>
                 <button className='page-btn'>5</button>
             </div>
 
             <button className='page-nav-btn'>
-                <img src={Right} alt='Next button'/>
-                {/* Next */}
+                { viewportWidth < 768  ? '' : 'Next' }
+                <Right role='img' aria-label='Next'/>
             </button>
-        </div>
+        </div>        
     );
 } 
