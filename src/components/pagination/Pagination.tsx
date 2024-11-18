7c909978289e03ed653c908b83ac2d65a8a2814d
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2024-11-18 13:56:20 +0200

    reverted scss.module to scss

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react';
-import styles from './pagination.module.scss'; 
+import './pagination.scss'; 
 import Right from '../../assets/images/icon-caret-right.svg?react';
 import Left from '../../assets/images/icon-caret-left.svg?react';
 
 // TODO: functionalitate butoane
 export default function Pagination () {
-    const [viewportWidth, setViewportWidth] = useState(window.innerWidth); 
-    
-    window.addEventListener('resize', () => {
-        setViewportWidth(window.innerWidth);
-    })
-
     return(
-        <div className={styles.pagination}>
-            <button className={styles.page_nav_btn}>
+        <div className="pagination">
+            <button className="page-nav-btn">
                 <Left role='img' aria-label='Previous'/>
-                { viewportWidth < 768 ? '' : 'Prev' }
-                {/* add text on prev and next buttons, when viewport width above tablet size */}
+                <span className='btn-text'>Prev</span>
             </button>
 
-            <div className={styles.all_page_btns}>
-                <button className={styles.page_btn}>1</button>
-                <button className={`${styles.page_btn} ${styles.active}`}>2</button>
-                <button className={styles.page_btn}>3</button>
-                <button className={styles.page_btn}>4</button>
-                <button className={styles.page_btn}>5</button>
+            <div className="all-page-btns">
+                <button className="page-btn">1</button>
+                <button className="page-btn active">2</button>
+                <button className="page-btn">3</button>
+                <button className="page-btn">4</button>
+                <button className="page-btn">5</button>
             </div>
 
-            <button className={styles.page_nav_btn}>
-                { viewportWidth < 768  ? '' : 'Next' }
+            <button className="page-nav-btn">
+                <span className='btn-text'>Prev</span>
                 <Right role='img' aria-label='Next'/>
             </button>
         </div>        
