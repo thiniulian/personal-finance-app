11b5e1f5e36393c07dc5b593140f05a7247f95fc
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2024-11-21 16:54:46 +0200

    mobile view for sidebar

--- src/components/sidebar/sidebarItem/SidebarItem.tsx
+++ src/components/sidebar/sidebarItem/SidebarItem.tsx
@@ -8,12 +8,14 @@ interface SidebarItemProps {
 
 export default function SidebarItem({isSelected, icon, label}: SidebarItemProps) {
     return(
-        <div className={isSelected ? "sidebar-item selected":"sidebar-item"}>
+        <button className={isSelected ? "sidebar-item selected":"sidebar-item"}>
             <span 
             className='sidebar-item-icon'
             dangerouslySetInnerHTML={{__html: icon }}
+            role="img"
+            aria-label={`icon for ${label}`}
             ></span>
             <span className='sidebar-item-label'>{label}</span>
-        </div>
+        </button>
     );
 };
