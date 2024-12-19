4ab4fc15d60fc5271b29fdc29eed05e8bb495228
Anca Matei <98110730+Anca2022@users.noreply.github.com>
2024-12-19 14:53:07 +0200

    dropdown stays in view

--- src/components/inputField/selectElement/SelectElement.tsx
+++ src/components/inputField/selectElement/SelectElement.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CarretDownIcon from "../../../assets/images/icon-caret-down.svg?react";
 import "../inputField.scss";
 
@@ -16,15 +16,49 @@ const transactionCategories = [
 export default function SelectElement({dropdownOpened, selectDecorator}:selectProps){
     const [tagColor, setTagColor] = useState(colorTags[0]);
     const [category, setCategory] = useState(transactionCategories[0]);
+    const [dropdownStyle, setDropdownStyle] = useState({});
+    const selectRef = useRef<null | HTMLDivElement>(null); 
+    const dropdownRef = useRef<null | HTMLDivElement>(null); 
 
     function transformColorName(color:string){
         // to change color name (ex: from 'navy-grey' to 'Navy grey')
         return color.replace("-", ' ').replace(color.charAt(0), color.charAt(0).toUpperCase());
     }
+    //to reposition dropdown if not in view when opening
+    function handlePosition(){
+        if(!selectRef.current || !dropdownRef.current) return; 
+        
+        const triggerRect = selectRef.current!.getBoundingClientRect();
+        const dropdownRect = dropdownRef.current!.getBoundingClientRect();
+        // Calculate available space
+        const spaceBelow = window.innerHeight - triggerRect.bottom;
+        const spaceAbove = triggerRect.top;
+        const dropdownHeight = dropdownRect.height;
+        // Position dropdown
+        const newStyle = {
+            top: spaceBelow < dropdownHeight && spaceAbove > dropdownHeight 
+            ? `calc(-${dropdownHeight}px - 8px)` // Position above
+            : '3.5rem'// Default position below
+        };
+        setDropdownStyle(newStyle);
+    }
+
+    useEffect(() => {
+        if (dropdownOpened) {
+          handlePosition();
+        }
+        const handleScrollOrResize = () => {
+            if (dropdownOpened) {
+            handlePosition();
+            }
+        };
+        window.addEventListener('scroll', handleScrollOrResize);
+        window.addEventListener('resize', handleScrollOrResize);
+    }, [dropdownOpened]);
 
     return (
         <>
-            <div className="select-element">
+            <div className="select-element" ref={selectRef}>
                 {selectDecorator == 'color tag'? 
                 transformColorName(tagColor) 
                 : 
@@ -34,7 +68,8 @@ export default function SelectElement({dropdownOpened, selectDecorator}:selectPr
             <span className={dropdownOpened? "icon-decorator reversed-icon-decorator" : "icon-decorator"}>
                 <CarretDownIcon/>
             </span>
-            <div className={dropdownOpened? "dropdown display-dropdown":"dropdown"}>
+            <div className={dropdownOpened? "dropdown display-dropdown":"dropdown"} ref={dropdownRef}
+                style={dropdownStyle}>
                 <ul>
                    {selectDecorator == "color tag" ? 
                         colorTags.map((color, index) => {
--- src/pages/transactions/Transactions.tsx
+++ src/pages/transactions/Transactions.tsx
@@ -7,17 +7,15 @@ export default function Transactions () {
     return (
         <section className="transactions">
             <h1>Transactions page</h1>
-
             {/* div for temporary styles */}
             <div 
             style={{margin: "1rem", display: "flex", flexDirection: "column", gap:"1rem", maxWidth: 380 }}
             >
             <InputField selectElement={true} fieldTitle={'Field with dropdown'} selectDecorator={"basic"}/>
-            <InputField selectElement={true} fieldTitle={'Field with color tag'} selectDecorator={"color tag"}/>
-            
             <InputField selectElement={false} fieldTitle={'Basic Field'} inputPlaceholder={'Placeholder'} helperText={'Min 3 char'} inputDecorator={"basic"}/>
             <InputField selectElement={false} fieldTitle={'Field with icon'} inputPlaceholder={'Placeholder'}  helperText={'Helper text'} inputDecorator={'icon'} onChange={handleChange}/>
             <InputField selectElement={false} fieldTitle={'Field with prefix'} inputPlaceholder={'Placeholder'} inputDecorator={'prefix'}/>
+            <InputField selectElement={true} fieldTitle={'Field with color tag'} selectDecorator={"color tag"}/>
             </div>
         </section>
     );
